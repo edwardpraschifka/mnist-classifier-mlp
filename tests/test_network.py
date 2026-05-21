742afbf15745741dedbccd4e687838299d9006d1
epraschifka <edwardpraschifka@gmail.com>
2026-05-21 10:46:06 +0930

    Update: Group tests by class
    Group tests by method being tested,
    in order to avoid naming conflicts

--- tests/test_network.py
+++ tests/test_network.py
@@ -3,25 +3,27 @@ import numpy as np
 
 from src.network import Network
 
-def test_network_dims():
-    """Create a 3-layer network with layers of size 3,4,2"""
+class TestConstructor:
+    def test_network_dims(self):
+        """Create a 3-layer network with layers of size 3,4,2"""
 
-    layers = np.array([3,4,2])
-    nw = Network(layers)
-
-    assert nw.size == 3
-
-    assert len(nw.weights) == 3
-    assert nw.weights[0].shape == (3,4)
-    assert nw.weights[1].shape == (4,2)
-    
-    assert len(nw.biases) == 3
-    assert len(nw.biases[0]) == 3
-    assert len(nw.biases[1]) == 4
-    assert len(nw.biases[2]) == 2
-
-def test_bad_type():
-    with pytest.raises(TypeError):
-        layers = list([])
+        layers = np.array([3,4,2])
         nw = Network(layers)
-    
+
+        assert nw.size == 3
+
+        assert(np.array_equal(layers, nw.layers))
+
+        assert len(nw.weights) == 3
+        assert nw.weights[0].shape == (3,4)
+        assert nw.weights[1].shape == (4,2)
+        
+        assert len(nw.biases) == 3
+        assert len(nw.biases[0]) == 3
+        assert len(nw.biases[1]) == 4
+        assert len(nw.biases[2]) == 2
+
+    def test_bad_type(self):
+        with pytest.raises(TypeError):
+            layers = list([])
+            nw = Network(layers)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,32 +3,33 @@ import numpy as np
 
 from src.utils import cost
 
-def test_cost_1():
-    y1 = np.array([1,2,3])
-    y2 = np.array([4,5,6])
+class TestCostFunction:
+    def test_cost_1(self):
+        y1 = np.array([1,2,3])
+        y2 = np.array([4,5,6])
 
-    out = cost(y1,y2)
-    assert out == 27
-
-def test_cost_2():
-    y1 = np.array([-1,2,-3])
-    y2 = np.array([4,-5,6])
-
-    out = cost(y1,y2)
-    assert out == 155
-
-def test_bad_type():
-    y1 = list([])
-    y2 = np.array([])
-
-    with pytest.raises(TypeError):
         out = cost(y1,y2)
-    
+        assert out == 27
 
-def test_diff_len():
-    y1 = np.array([1,2,3])
-    y2 = np.array([1,2])
+    def test_cost_2(self):
+        y1 = np.array([-1,2,-3])
+        y2 = np.array([4,-5,6])
 
-    with pytest.raises(ValueError):
         out = cost(y1,y2)
+        assert out == 155
+
+    def test_bad_type(self):
+        y1 = list([])
+        y2 = np.array([])
+
+        with pytest.raises(TypeError):
+            out = cost(y1,y2)
+        
+
+    def test_diff_len(self):
+        y1 = np.array([1,2,3])
+        y2 = np.array([1,2])
+
+        with pytest.raises(ValueError):
+            out = cost(y1,y2)
 
