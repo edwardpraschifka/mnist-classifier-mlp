56fcfb66d93ab230b0e6e73d0d2fbbc57285fccb
epraschifka <edwardpraschifka@gmail.com>
2026-06-03 17:03:16 +0930

    Add: activation, cost_fn parameters to train.py
    Allow users to specify activation and
    cost function as params to train.py

--- src/train.py
+++ src/train.py
@@ -1,13 +1,19 @@
 import argparse
 
+from src.activations import Sigmoid
+from src.cost_functions import QuadraticCost, CrossEntropyCost
 from src.network import Network
 from src.utils import get_mnist_data
 
-def train_mnist(batch_size: int, learning_rate: float, epochs: int):
+def train_mnist(batch_size: int, learning_rate: float, epochs: int, activation_fn: int, cost_fn: int):
     """Train model on MNIST dataset"""
 
+    activation_fn = {0: Sigmoid}[activation_fn]
+    cost_fn = {0: QuadraticCost, 1: CrossEntropyCost}[cost_fn]
+    
+
     layers = [784, 128, 64, 10]
-    nw = Network(layers)
+    nw = Network(layers, activation_fn, cost_fn)
 
     (X_train, Y_train, _, _) = get_mnist_data()
     nw.gradient_descent(X_train.T, Y_train.T, batch_size, learning_rate, epochs, display=True)
@@ -21,8 +27,11 @@ if __name__ == "__main__":
     parser.add_argument("-bs", type=int, default=32, help="Batch size (default: 32)")
     parser.add_argument("-lr", type=float, default=0.01, help="Learning rate (default: 0.01)")
     parser.add_argument("-e", type=int, default=30, help="Number of training epochs (default: 30)")
+    parser.add_argument("-a", type=int, default=0, help="Activation function (default: sigmoid)")
+    parser.add_argument("-c", type=int, default=0, help="Cost function (default: quadratic)")
     parser.add_argument("-o", type=str, default="network.pkl", help="Output file (default: 'network.pkl')")
     args = parser.parse_args()
 
-    nw = train_mnist(batch_size = args.bs, learning_rate = args.lr, epochs = args.e)
+    nw = train_mnist(batch_size = args.bs, learning_rate = args.lr, epochs = args.e, 
+                     activation_fn = args.a, cost_fn = args.c)
     nw.save(filename = args.o)
