cfbf87046fde2c97f4f47f14b9e265d83aa656b4
epraschifka <edwardpraschifka@gmail.com>
2026-06-03 16:31:43 +0930

    Remove: test_activations.py and test_cost_fns.py files
    Move cost function and activation function
    tests into existing test files

--- tests/test_network.py
+++ tests/test_network.py
@@ -3,11 +3,29 @@ import numpy as np
 import torch
 import os
 
+from src.activations import Sigmoid
 from src.network import Network
 from src.utils import accuracy, get_mnist_data
-from tests.test_cost_fns import backprop_torch
-from tests.utils import feedforward_torch
+from tests.utils import feedforward_torch, backprop_torch
 from src.cost_functions import QuadraticCost, CrossEntropyCost
+
+
+class TestSigmoid:
+    def test_output_int(self):
+        z = 2
+        sig = Sigmoid()
+        out = sig.forward(z)
+
+        assert round(out,2) == 0.88
+    
+    def test_output_vec(self):
+        z = np.array([1,2,3])
+        sig = Sigmoid()
+        out = sig.forward(z)
+
+        expected_out = np.array([0.73, 0.88, 0.95])
+
+        assert np.array_equal(np.round(out,2), expected_out)
 
 class TestConstructor:
 
